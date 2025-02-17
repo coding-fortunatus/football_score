3a9007bed4c579b6412a9075a0100c6c78b45b13
coding-fortunatus <fortunatusoluwadara@gmail.com>
2025-02-18 00:20:09 +0100

    Implement index and show methods in OfficialController to fetch and display officials

--- app/Http/Controllers/OfficialController.php
+++ app/Http/Controllers/OfficialController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Action\Officials\FetchAllOfficials;
+use App\Action\Officials\ShowOfficialById;
 use Illuminate\Http\Request;
 
 class OfficialController extends Controller
 {
-    public function index() {}
-    public function show($id) {}
+    public function index(FetchAllOfficials $action)
+    {
+        $officials = $action->execute();
+        $message = $officials ? false : true;
+        return view('admin.official', compact('officials', 'message'));
+    }
+    public function show($id, ShowOfficialById $action)
+    {
+        $official = $action->execute($id);
+        $message  = $official ? false : true;
+        return view('admin.single_officlal', compact('message', 'official'));
+    }
     public function store() {}
     public function update($id) {}
     public function destroy($id) {}
